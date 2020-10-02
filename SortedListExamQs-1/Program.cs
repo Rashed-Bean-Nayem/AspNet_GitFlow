9ef490082768498105bc2e7c46976a1097a5310c
Rashed <rashedamlan02@gmail.com>
2020-10-02 20:45:39 +0600

    added exm answer

---
+++ SortedListExamQs-1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+
+namespace SortedListExamQs_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList NumberSortedList = new SortedList();
+           
+            NumberSortedList.Add(4, "Four");
+            NumberSortedList.Add(1, "One");
+            NumberSortedList.Add(3, "Three");
+            NumberSortedList.Add(7, "Seven");
+           
+          
+          
+            foreach (var item in NumberSortedList)
+            {
+                Console.WriteLine($"Key: {item.Key}, Value: {item.Value}");
+            }
+        }
+    }
+}
---
+++ qs-2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+
+namespace qs_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queueList = new Queue<int>();
+           
+            queueList.Enqueue(50);
+            queueList.Enqueue(10);
+            queueList.Enqueue(30);
+            queueList.Enqueue(10);
+
+            queueList.Dequeue();
+          
+
+            Console.WriteLine("Peek: " + queueList.Peek());
+
+            foreach (var item in queueList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Peek: " + queueList.Peek());
+        }
+    }
+}
